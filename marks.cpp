4c83f26d259b51310ace40f090372f18d8f28f59
adhar-mirp <adhar-mirp@mit.edu>
2017-05-31 10:15:19 +0530

    ADDIND FILE a.out

---
+++ marks.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a, b,mark ,c;
+  cout << "Enter the cutoffs for a, b, c respectively" << endl;
+  cin >> a >> b >> c;
+  cout << "Enter the student's marks" << endl;
+  cin >> marks;
+  if ((a>b) && (a>c) && (b>c));
+{
+  cout << "cutoffs are valid" <<endl ;
+if (marks > a);
+ cout << "Grade A"<<endl ;
+else if (marks > b)
+  cout << "Grade B"<<endl ;
+else if (marks > c) ;
+ cout << "Grade C"<<endl ;
+else (marks < c) ;
+ cout << "Grade D"<<endl ;
+ }
+ return 0;
+}
