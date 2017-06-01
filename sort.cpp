e6efc4bbc42fd0f56fbd8e7774cfba38dfff76e0
pavan <kpsair-mirp@mit.edu>
2017-06-01 07:02:43 +0530

    unfinished day 4

---
+++ sort.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int N,k;
+cout<<"Enter how many numbers do you want to sort."<<endl;
+cin>>N;
+int arr[N];
+for (int i=1;i<=N;i++){
+   cout<<"Give you entry number "<<i<<endl;
+   cin>>arr[i];
+   }
+
