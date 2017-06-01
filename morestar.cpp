ecaf02e2334ac5b4b17a32ba4be14f682bbca4ee
pavan <kpsair-mirp@mit.edu>
2017-06-02 04:19:34 +0530

    more starsgit add *

---
+++ morestar.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int N,k=0,m=0;
+cout<<"Enter a Number"<<endl;
+cin>>N;
+while (k<=N) {
+     for (int l=k;l<=(2*N-1);l++){
+       cout<<"  ";
+       }
+     for (m=1;m<=(2*k-1);m++)
+     { 
+       cout<<"* ";
+      }
+     cout<<""<<endl;
+     k++; 
+}
+return 0;
+}
