f80ccf857125a3222b05e468cbd5d295570b8023
pavan <kpsair-mirp@mit.edu>
2017-06-05 04:24:14 +0530

    Donegit add *

--- altstar.cpp
+++ altstar.cpp
@@ -2,38 +2,17 @@
 using namespace std;
 int main(){
 int columns,rows;
-int c,r;
 cout<<"Enter the number of columns and rows respectively.\n";
 cin>>columns;
 cin>>rows;
-c=columns/2;
-r=rows/2;
-if (c%1==0) {//the number is even
-  if (r%1==0){
-  for (int j=1;j<=r;j++){
-     for (int i=1;i<=r;i++){
-        cout<<"* ";
-     }
-     cout<<""<<endl;
-      for (int i=1;i<=r;i++){
-        cout<<" *";
-     }
-      
- cout<<""<<endl;  
-    }
-  }
-  else{
-   for (int j=1;j<=r;j++){
-    for (int i=1;i<=(r+1);i++){
-        cout<<"* ";
-     }
-     cout<<""<<endl;
-      for (int i=1;i<=r;i++){
-        cout<<" *";
-     }
-   }
-   for (int j=1;j<=(r+1);j++){
-  cout<<"* "
-  }
+for (int i=1;i<=rows;i++){
+    for (int j=1;j<=columns;j++){
+         if ((i+j)%2==0)
+             cout<<"*";
+         else
+             cout<<" ";
+          }
+         cout<<""<<endl;
+      }
 return 0;
 }
